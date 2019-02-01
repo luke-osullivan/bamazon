46dce0ab0b267288dd481aff0e60060ebd191121
Luke O'Sullivan <luke@Lukes-MacBook.home>
2019-02-01 03:28:10 -0500

    mysql

---
+++ seeds.sql
@@ -0,0 +1,49 @@
+INSERT INTO products
+  (product_name, department_name, price, stock)
+VALUES
+  ('Laptop', 'Electronics', '500.00', 2);
+  
+INSERT INTO products
+  (product_name, department_name, price, stock)
+VALUES
+  ('Mouse', 'Electronics', '25.00', 5);
+
+INSERT INTO products
+  (product_name, department_name, price, stock)
+VALUES
+  ('Keyboard', 'Electronics', '30.00', 5);
+  
+  INSERT INTO products
+  (product_name, department_name, price, stock)
+VALUES
+  ('Sweater', 'Clothing', '37.50', 10);
+  
+  INSERT INTO products
+  (product_name, department_name, price, stock)
+VALUES
+  ('Hat', 'Clothing', '4.99', 5);
+  
+INSERT INTO products
+  (product_name, department_name, price, stock)
+VALUES
+  ('Magazine', 'Books', '5.00', 10);
+  
+INSERT INTO products
+  (product_name, department_name, price, stock)
+VALUES
+  ('Novel', 'Books', '10.00', 10);
+  
+INSERT INTO products
+  (product_name, department_name, price, stock)
+VALUES
+  ('Audiobook', 'Books', '2.50', 10);
+  
+INSERT INTO products
+  (product_name, department_name, price, stock)
+VALUES
+  ('Bracelet', 'Jewelry', '50.00', 5);
+  
+INSERT INTO products
+  (product_name, department_name, price, stock)
+VALUES
+  ('Necklace', 'Jewelry', '100.00', 5);
